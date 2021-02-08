2a0f28d413ce5461dc2fd9848249739c0bd28005
Arie Lev <levinsonarie@gmail.com>
2021-02-08 19:50:24 +0200

    add version 0.0.1

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nalkinscloud_mqtt_python_client",
+    version="0.0.1",
+    author="Arie Lev",
+    author_email="levinson.arie@gmail.com",
+    description="Simple MQTT client implementation that fits nalkinscloud projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArieLevs/nalkinscloud-mqtt-python-client",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'paho-mqtt==1.5.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
