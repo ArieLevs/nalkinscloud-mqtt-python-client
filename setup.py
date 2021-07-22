f004f1d512531114c88dc03ffa6a31914a1d3e64
Arie Lev <levinsonarie@gmail.com>
2021-07-22 19:40:31 +0300

    move req to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
+from pkg_resources import parse_requirements
+
+with open("requirements.txt") as f:
+    requirements = [str(r) for r in parse_requirements(f)]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="nalkinscloud_mqtt_python_client",
-    version="0.0.3",
+    version="1.0.0",
     author="Arie Lev",
     author_email="levinson.arie@gmail.com",
     description="Simple MQTT client implementation that fits nalkinscloud projects",
@@ -13,9 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ArieLevs/nalkinscloud-mqtt-python-client",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'paho-mqtt==1.5.1',
-    ],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
